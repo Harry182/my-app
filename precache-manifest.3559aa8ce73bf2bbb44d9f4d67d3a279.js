6c77f9a93d46c6657d9f3f522c5f76e193365c39
Harry182 <markus44p@gmail.com>
2020-04-28 21:37:03 -0500

    Updates

---
+++ precache-manifest.3559aa8ce73bf2bbb44d9f4d67d3a279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4f439f16e55f7b5448d55dc349d7b7",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ae4814c24a8f94128d1c",
+    "url": "/my-app/static/css/main.d9d91a86.chunk.css"
+  },
+  {
+    "revision": "f0f6b96b5c9935c892a6",
+    "url": "/my-app/static/js/2.b13ae2ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.b13ae2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4814c24a8f94128d1c",
+    "url": "/my-app/static/js/main.7e28b87a.chunk.js"
+  },
+  {
+    "revision": "6b90711d51ae396ac389",
+    "url": "/my-app/static/js/runtime-main.8c451e69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
